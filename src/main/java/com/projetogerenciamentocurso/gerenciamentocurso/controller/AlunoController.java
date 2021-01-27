e97bd84b7808119442967ae852b1ae133c5fefa9
vitor.jose <vitor.jose@JV060104815.jv01.local>
2021-01-27 16:32:11 -0300

    Retirando a importação

--- src/main/java/com/projetogerenciamentocurso/gerenciamentocurso/controller/AlunoController.java
+++ src/main/java/com/projetogerenciamentocurso/gerenciamentocurso/controller/AlunoController.java
@@ -1,7 +1,5 @@
 package com.projetogerenciamentocurso.gerenciamentocurso.controller;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
