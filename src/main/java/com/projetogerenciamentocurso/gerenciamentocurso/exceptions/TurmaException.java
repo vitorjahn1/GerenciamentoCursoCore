90f1dd98c97a07e1e5b7aaf27763b657958fdfa7
vitor.jose <vitor.jose@JV060104815.mshome.net>
2021-01-29 16:55:43 -0300

    Colocando @ResponseStatus(code=HttpStatus.NOT_FOUND),nas classes de exception

--- src/main/java/com/projetogerenciamentocurso/gerenciamentocurso/exceptions/TurmaException.java
+++ src/main/java/com/projetogerenciamentocurso/gerenciamentocurso/exceptions/TurmaException.java
@@ -1,5 +1,9 @@
 package com.projetogerenciamentocurso.gerenciamentocurso.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code=HttpStatus.NOT_FOUND)
 public class TurmaException extends RuntimeException{
 
 	private static final long serialVersionUID = 5891289913524251413L;
