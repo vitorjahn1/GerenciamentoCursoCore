4982fe06d39d8690868298b1a1a7038825f2c2b9
vitor.jose <vitor.jose@JV060104815.jv01.local>
2021-02-10 16:02:54 -0300

    correção

--- src/main/java/com/projetogerenciamentocurso/gerenciamentocurso/GerenciamentoCursoApplication.java
+++ src/main/java/com/projetogerenciamentocurso/gerenciamentocurso/GerenciamentoCursoApplication.java
@@ -24,7 +24,7 @@ public class GerenciamentoCursoApplication {
 
 	public static final String QUEUE_TURMA_ATUALIZAR = "turmaAtualizar";
 	public static final String QUEUE_TURMA_CRIAR = "turmaCriar";
-	public static final String QUEUE_TURMA_DELETAR = "turmaCriar";
+	public static final String QUEUE_TURMA_DELETAR = "turmaDeletar";
 	
 	public static final String QUEUE_PROFESSOR_ATUALIZAR = "professorAtualizar";
 	public static final String QUEUE_PROFESSOR_CRIAR = "professorCriar";
